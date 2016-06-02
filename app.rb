1ed76311442ca8dc0b0ad80c4c840bef6692499d
shyazusa <shy.azusa@gmail.com>
2016-06-02 23:39:42 +0900

    Remove /

--- app.rb
+++ app.rb
@@ -3,10 +3,6 @@ require 'indico'
 
 require './setting'
 
-get '/' do
-  Indico.sentiment_hq("I love writing code!").to_json
-end
-
 get '/img' do
   Indico.content_filtering(params['url']).to_json
 end
